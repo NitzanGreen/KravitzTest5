a09af1ce13bbb374c42c276f9557ce75181277b0
NitzanGreen <pakamausi@gmail.com>
2019-05-01 12:42:25 +0300

    Add files via upload

---
+++ Kravitz_V5/jScripts/JavaScript.js
@@ -0,0 +1,27 @@
+﻿var navItems = document.querySelectorAll(".mobile-bottom-nav__item");
+navItems.forEach(function (e, i) {
+    e.addEventListener("click", function (e) {
+        navItems.forEach(function (e2, i2) {
+            e2.classList.remove("mobile-bottom-nav__item--active");
+        })
+        this.classList.add("mobile-bottom-nav__item--active");
+    });
+});
+
+
+
+
+//var acc = document.getElementsByClassName("accordion");
+//var i;
+
+//for (i = 0; i < acc.length; i++) {
+//    acc[i].addEventListener("click", function () {
+//        this.classList.toggle("active");
+//        var panel = this.nextElementSibling;
+//        if (panel.style.maxHeight) {
+//            panel.style.maxHeight = null;
+//        } else {
+//            panel.style.maxHeight = panel.scrollHeight + "px";
+//        }
+//    });
+//}
