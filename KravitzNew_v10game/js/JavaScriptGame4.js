41155b57ca253b56bd818360e01d2a689484ac7e
NitzanGreen <pakamausi@gmail.com>
2019-09-02 23:10:12 +0300

    Update JavaScriptGame4.js

--- KravitzNew_v10game/js/JavaScriptGame4.js
+++ KravitzNew_v10game/js/JavaScriptGame4.js
@@ -18,7 +18,7 @@ function sumFunc() {
 
     $.ajax({
         dataType: 'json',
-        url: '/json/myJson.json',
+        url: 'json/myJson.json',
         async: false,
         success: function (data) {
 
@@ -46,7 +46,7 @@ function Game1Func() {
 
     $.ajax({
         dataType: 'json',
-        url: '/json/myJson.json',
+        url: 'json/myJson.json',
         async: false,
         success: function (data) {
             document.getElementById("ProductExplanationAlonePart").innerHTML = data.project.ProductExplanationAlonePart.ProductExplanation4;
